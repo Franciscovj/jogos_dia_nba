325e1e6690b1639fecf15258e844009ed0a6e411
Franciscovj <107822397+Franciscovj@users.noreply.github.com>
2023-03-13 14:54:09 -0300

    Update app.py

--- app/app.py
+++ app/app.py
@@ -38,14 +38,20 @@ df = load_data(selected_league)
 # Sidebar - Columns selection
 sorted_unique_column = df.columns.to_list()
 
-selected_column = st.sidebar.multiselect('Columns', sorted_unique_column, ['Liga', 'Date', 'Home', 'Away', 'Goals_H_HT', 'Goals_A_HT', 'FT_Goals_H', 'FT_Goals_A', 'FT_Odds_H', 'FT_Odds_D', 'FT_Odds_A', 'HT_Odds_H', 'HT_Odds_D', 'HT_Odds_A', 'FT_Odds_DC_X2', 'HT_Odds_Under05', 'HT_Odds_Under15', 'FT_Odds_Over25', 'FT_Odds_under25', 'FT_Odds_Under35', 'FT_Odds_BTTS_Yes',
-                                                                           'FT_Odds_BTTS_No', 'Result_HT', 'FT_Result', 'Total_Goals_FT', 'Total_Goals_HT', 'p_H_HT', 'p_D_HT', 'p_A_HT', 'Pontos_H_ht', 'Pontos_A_ht', 'PPJ_H_ht', 'PPJ_A_ht', 'SG_H_ht', 'SG_A_ht', 'DP_Pontos_H_ht', 'DP_Pontos_A_ht', 'CV_Pontos_H_ht', 'CV_Pontos_A_ht', 'VG_H_ht', 'VG_A_ht', 'Media_VG_H_ht', 
-                                                                           'Media_VG_A_ht', 'DesvPad_VG_H_ht', 'DesvPad_VG_A_ht', 'CV_VG_H_ht', 'CV_VG_A_ht', 'CG_H_ht', 'CG_A_ht', 'Media_CG_H_ht', 'Media_CG_A_ht', 'DesvPad_CG_H_ht', 'DesvPad_CG_A_ht', 'CV_CG_H_ht', 'CV_CG_A_ht', 'p_H', 'p_D', 'p_A', 'p_Over', 'p_Under', 'p_H__p_D', 'p_H__p_A', 'p_D__p_A', 'p_D__p_Over', 
-                                                                           'p_D__p_Under', 'p_Over_mh', 'p_Under_ma', 'p_Over_ma', 'p_Under_mh', 'CV_HDA', 'CV_HD', 'CV_HA', 'CV_DA', 'CV_DO', 'Pontos_H', 'Pontos_A', 'PPJ_H', 'PPJ_A', 'SG_H', 'SG_A', 'DP_Pontos_H', 'DP_Pontos_A', 'CV_Pontos_H', 'CV_Pontos_A', 'VG_H', 'VG_A', 'Media_VG_H', 'Media_VG_A', 'DesvPad_VG_H',
-                                                                           'DesvPad_VG_A', 'CV_VG_H', 'CV_VG_A', 'CG_H', 'CG_A', 'Media_CG_H', 'Media_CG_A', 'DesvPad_CG_H', 'DesvPad_CG_A', 'CV_CG_H', 'CV_CG_A', 'GM_H', 'GM_A', 'Media_GM_H', 'Media_GM_A', 'DP_GM_H', 'DP_GM_A', 'CV_GM_H', 'CV_GM_A', 'GS_H', 'GS_A', 'Media_GS_H', 'Media_GS_A', 'DP_GS_H', 'DP_GS_A', 'CV_GS_H',
-                                                                           'CV_GS_A', 'Media_p_H', 'Media_p_A', 'DP_p_H', 'DP_p_A', 'CG_H_XP', 'CG_A_XP', 'DP_CG_H_XP', 'DP_CG_A_XP', 'VG_H_XP', 'VG_A_XP', 'DP_VG_H_XP', 'DP_VG_A_XP', 'GM_H_XP', 'GM_A_XP', 'CV_GM_H_XP', 'CV_GM_A_XP', 'GS_H_XP', 'GS_A_XP', 'CV_GS_H_XP', 'CV_GS_A_XP', 'gols_sofridos_home', 'gols_sofridos_away', 
-                                                                           'Over05_FT', 'Over05_HT', 'Under05_HT', 'Under15_HT', 'Over25_FT', 'Under25_FT', 'Under35_FT', 'Home_Win', 'Away_Win', 'Away_X2', 'home_X1', 'Draw', 'BTTS', 'BTTS_NO', 'Porc_Over05HT_Home', 'Porc_Over05HT_Away', 'Porc_Under15HT_Home', 'Porc_Under15HT_Away', 'Porc_Over05FT_Home', 'Porc_Over05FT_Away',
-                                                                           'Porc_draw_Home', 'Porc_draw_Away', 'Porc_Over25FT_Home', 'Porc_Over25FT_Away', 'Porc_BTTS_Home', 'Porc_BTTS_Away'])
+selected_column = st.sidebar.multiselect('Columns', sorted_unique_column, [
+ 'Liga', 'Date', 'Home', 'Away', 'Goals_H_HT', 'Goals_A_HT', 'FT_Goals_H', 'FT_Goals_A', 'FT_Odds_H', 'FT_Odds_D', 'FT_Odds_A', 'HT_Odds_H', 'HT_Odds_D', 'HT_Odds_A', 'FT_Odds_DC_X2', 
+ 'HT_Odds_Under05', 'HT_Odds_Under15', 'FT_Odds_Over25', 'FT_Odds_under25', 'FT_Odds_Under35', 'FT_Odds_BTTS_Yes',
+ 'FT_Odds_BTTS_No', 'Result_HT', 'FT_Result', 'Total_Goals_FT','Total_Goals_HT','p_H', 'p_D', 'p_A', 'p_Over', 'p_Under',
+ 'p_H__p_D', 'p_H__p_A', 'p_D__p_A', 'p_D__p_Over','p_D__p_Under', 'CV_HDA', 'CV_HD', 'CV_HA', 'CV_DA', 'CV_DO', 'Pontos_H', 'Pontos_A', 'PPJ_H',
+ 'PPJ_A', 'SG_H', 'SG_A', 'DP_Pontos_H', 'DP_Pontos_A', 'CV_Pontos_H', 'CV_Pontos_A', 'VG_H', 'VG_A', 'Media_VG_H', 'Media_VG_A', 'DesvPad_VG_H','DesvPad_VG_A', 'CV_VG_H', 'CV_VG_A',
+ 'CG_H', 'CG_A', 'Media_CG_H', 'Media_CG_A', 'DesvPad_CG_H', 'DesvPad_CG_A', 'CV_CG_H',
+ 'CV_CG_A', 'GM_H', 'GM_A', 'Media_GM_H', 'Media_GM_A', 'DP_GM_H', 'DP_GM_A', 'CV_GM_H', 'CV_GM_A', 'GS_H', 'GS_A', 'Media_GS_H', 'Media_GS_A', 'DP_GS_H', 'DP_GS_A', 'CV_GS_H',
+ 'CV_GS_A', 'Media_p_H', 'Media_p_A', 'DP_p_H', 'DP_p_A', 'CG_H_XP', 'CG_A_XP', 'DP_CG_H_XP', 'DP_CG_A_XP', 'VG_H_XP', 'VG_A_XP', 'DP_VG_H_XP', 'DP_VG_A_XP', 'GM_H_XP', 'GM_A_XP', 'CV_GM_H_XP',
+ 'CV_GM_A_XP', 'GS_H_XP', 'GS_A_XP', 'CV_GS_H_XP', 'CV_GS_A_XP', 'gols_sofridos_home', 'gols_sofridos_away', 
+ 'Over05_FT', 'Over05_HT', 'Under05_HT', 'Under15_HT', 'Over25_FT', 'Under25_FT', 'Under35_FT', 'Home_Win', 
+ 'Away_Win', 'Away_X2', 'home_X1', 'Draw', 'BTTS', 'BTTS_NO', 'Porc_Over05HT_Home',
+ 'Porc_Over05HT_Away', 'Porc_Under15HT_Home', 'Porc_Under15HT_Away', 'Porc_Over05FT_Home', 'Porc_Over05FT_Away',
+ 'Porc_draw_Home', 'Porc_draw_Away', 'Porc_Over25FT_Home', 'Porc_Over25FT_Away', 'Porc_BTTS_Home', 'Porc_BTTS_Away'])
 
 # Sidebar - Team selection
 sorted_unique_team = sorted(df.Home.unique())
