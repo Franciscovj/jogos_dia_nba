948936a6374226db48cad1a44e6bef2e53d831bd
Franciscovj <107822397+Franciscovj@users.noreply.github.com>
2023-03-13 16:13:57 -0300

    Update app.py

--- app/app.py
+++ app/app.py
@@ -40,16 +40,8 @@ sorted_unique_column = df.columns.to_list()
 
 selected_column = st.sidebar.multiselect('Columns', sorted_unique_column, [
  'Liga', 'Date', 'Home', 'Away', 'Goals_H_HT', 'Goals_A_HT', 'FT_Goals_H', 'FT_Goals_A', 'FT_Odds_H', 'FT_Odds_D', 'FT_Odds_A', 'HT_Odds_H', 'HT_Odds_D', 'HT_Odds_A', 'FT_Odds_DC_X2', 
- 'HT_Odds_Under05', 'HT_Odds_Under15', 'FT_Odds_Over25', 'FT_Odds_under25', 'FT_Odds_Under35', 'FT_Odds_BTTS_Yes',
- 'FT_Odds_BTTS_No', 'Result_HT', 'FT_Result', 'Total_Goals_FT','Total_Goals_HT','p_H', 'p_D', 'p_A', 'p_Over', 'p_Under',
- 'p_H__p_D', 'p_H__p_A', 'p_D__p_A', 'p_D__p_Over','p_D__p_Under', 'CV_HDA', 'CV_HD', 'CV_HA', 'CV_DA', 'CV_DO', 'Pontos_H', 'Pontos_A', 'PPJ_H',
- 'PPJ_A', 'SG_H', 'SG_A', 'DP_Pontos_H', 'DP_Pontos_A', 'CV_Pontos_H', 'CV_Pontos_A', 'VG_H', 'VG_A', 'Media_VG_H', 'Media_VG_A', 'DesvPad_VG_H','DesvPad_VG_A', 'CV_VG_H', 'CV_VG_A',
- 'CG_H', 'CG_A', 'Media_CG_H', 'Media_CG_A', 'DesvPad_CG_H', 'DesvPad_CG_A', 'CV_CG_H',
- 'CV_CG_A', 'GM_H', 'GM_A', 'Media_GM_H', 'Media_GM_A', 'DP_GM_H', 'DP_GM_A', 'CV_GM_H', 'CV_GM_A', 'GS_H', 'GS_A', 'Media_GS_H', 'Media_GS_A', 'DP_GS_H', 'DP_GS_A', 'CV_GS_H',
- 'CV_GS_A', 'Media_p_H', 'Media_p_A', 'DP_p_H', 'DP_p_A', 'CG_H_XP', 'CG_A_XP', 'DP_CG_H_XP', 'DP_CG_A_XP', 'VG_H_XP', 'VG_A_XP', 'DP_VG_H_XP', 'DP_VG_A_XP', 'GM_H_XP', 'GM_A_XP', 'CV_GM_H_XP',
- 'CV_GM_A_XP', 'GS_H_XP', 'GS_A_XP', 'CV_GS_H_XP', 'CV_GS_A_XP', 'gols_sofridos_home', 'gols_sofridos_away', 
- 'Over05_FT', 'Over05_HT', 'Under05_HT', 'Under15_HT', 'Over25_FT', 'Under25_FT', 'Under35_FT', 'Home_Win', 
- 'Away_Win', 'Away_X2', 'home_X1', 'Draw', 'BTTS', 'BTTS_NO'])
+ 'HT_Odds_Under05', 'HT_Odds_Under15', 'FT_Odds_Over25', 'FT_Odds_under25', 'FT_Odds_Under35', 'FT_Odds_BTTS_Yes'
+])
 
 # Sidebar - Team selection
 sorted_unique_team = sorted(df.Home.unique())
