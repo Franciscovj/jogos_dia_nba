461d35a0ae22b32dc467bc34f89a981ba9f7e24d
Franciscovj <107822397+Franciscovj@users.noreply.github.com>
2023-03-13 11:54:58 -0300

    Update app.py

--- app/app.py
+++ app/app.py
@@ -24,7 +24,7 @@ def load_data(league):
   
   url = "'https://github.com/Franciscovj/jogos_dia_nba/blob/main/dash_23.csv?raw=true"
   data = pd.read_csv(url)
-  data = data[data['League'] == selected_league]
+  data = data[data['Liga'] == selected_league]
     
   return data
 
