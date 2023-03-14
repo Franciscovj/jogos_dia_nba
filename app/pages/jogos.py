6f5e624c25cd1272648606b1a20d78474dc620c0
Franciscovj <107822397+Franciscovj@users.noreply.github.com>
2023-03-14 15:32:32 -0300

    Update jogos.py

--- app/pages/jogos.py
+++ app/pages/jogos.py
@@ -29,8 +29,9 @@ valor_maximo = float(df_jogos['FT_Odd_H'].max())
 # Cria controles deslizantes para definir os valores mínimo e máximo
 #valor_minimo = st.slider('Valor mínimo', float(df_jogos['FT_Odd_H'].min()), float(df_jogos['FT_Odd_H'].max()), value=valor_minimo)
 #valor_maximo = st.slider('Valor máximo', float(df_jogos['FT_Odd_H'].min()), float(df_jogos['FT_Odd_H'].max()), value=valor_maximo)
-valor_minimo = st.slider('Valor mínimo', float(df_jogos['FT_Odd_H'].min()), float(df_jogos['FT_Odd_H'].max()), value=valor_minimo, width=300)
-valor_maximo = st.slider('Valor máximo', float(df_jogos['FT_Odd_H'].min()), float(df_jogos['FT_Odd_H'].max()), value=valor_maximo, width=300)
+valor_minimo = st.slider('Valor mínimo', float(df_jogos['FT_Odd_H'].min()), float(df_jogos['FT_Odd_H'].max()), value=valor_minimo, format="%f", step=0.01)
+valor_maximo = st.slider('Valor máximo', float(df_jogos['FT_Odd_H'].min()), float(df_jogos['FT_Odd_H'].max()), value=valor_maximo, format="%f", step=0.01)
+
 
 
 # Filtra o DataFrame com base nos valores mínimos e máximos selecionados nos controles deslizantes
