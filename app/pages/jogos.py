9ae486dc751714962883c3640fd167838a2904f7
Franciscovj <107822397+Franciscovj@users.noreply.github.com>
2023-03-14 18:05:50 -0300

    Update jogos.py

--- app/pages/jogos.py
+++ app/pages/jogos.py
@@ -10,7 +10,7 @@ st.set_page_config(
     layout="wide",
     initial_sidebar_state="expanded",
     # Define a cor de fundo da página
-    page_bg_color="#00FF00",
+    page_bg_color="#ff8080",
     # Define a cor da barra lateral
     sidebar_bg_color="#0000FF",
 )
