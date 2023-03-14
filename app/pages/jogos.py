9bd228c28a906c4441c35b0df730931ecdcfd419
Franciscovj <107822397+Franciscovj@users.noreply.github.com>
2023-03-14 14:50:49 -0300

    Update jogos.py

--- app/pages/jogos.py
+++ app/pages/jogos.py
@@ -20,8 +20,9 @@ def load_data_jogos():
 
 df_jogos = load_data_jogos()
 # Define o valor mínimo e máximo do controle deslizante com base na coluna "FT_Odd_H" do DataFrame
-valor_minimo = 0
+valor_minimo = float(0)
 valor_maximo = float(df_jogos['FT_Odd_H'].max())
+
 
 
 
