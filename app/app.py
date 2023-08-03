ad410da35099d03370122425e527220a801b60f4
Franciscovj <107822397+Franciscovj@users.noreply.github.com>
2023-08-03 18:27:27 -0300

    Update app.py

--- app/app.py
+++ app/app.py
@@ -31,7 +31,7 @@ selected_league = st.sidebar.selectbox('League',['BOLIVIA - DIVISION PROFESIONAL
 def load_data(league):
   
   
-  url = "https://github.com/Franciscovj/jogos_dia_nba/blob/main/dados_exportados.xlsx?raw=true"
+  url = "dados_exportados.xlsx"
   data = pd.read_excel(url)
   data = data[data['Liga'] == selected_league]
     
