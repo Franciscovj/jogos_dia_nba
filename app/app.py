d3d53f95793c3d28007bd3188c1a3b0631f78c55
Franciscovj <107822397+Franciscovj@users.noreply.github.com>
2023-08-04 17:07:35 -0300

    Update app.py

--- app/app.py
+++ app/app.py
@@ -4,35 +4,24 @@ import numpy as np
 import datetime
 from datetime import date
 
-
 st.title("Web App fvj Stats")
 st.markdown(
-        '''
-# Boas vindas ao meu app fvj Stats .
-boas analises!!!!!!!!!!
-'''
-    )
-                                                                                
+    '''
+    # Boas vindas ao meu app fvj Stats.
+    Boas análises!
+    '''
+)
+
 st.sidebar.header("Leagues")
-selected_league = st.sidebar.selectbox('League',['ALEMANHA - 2. BUNDESLIGA', 'ALEMANHA - BUNDESLIGA', 'BRASIL - SÉRIE A', 'BRASIL - SÉRIE B', 'BRASIL - SÉRIE C', 'BÉLGICA - LIGA JUPILER', 'ESPANHA - LALIGA', 'ESPANHA - LALIGA2',
-                                                 'FRANÇA - LIGUE 1', 'INGLATERRA - PREMIER LEAGUE', 'ITÁLIA - SERIE A', 'ITÁLIA - SERIE B', 'JAPÃO - LIGA J1', 'MÉXICO - LIGA MX', 'NORUEGA - ELITESERIEN', 'PORTUGAL - LIGA PORTUGAL', 
-                                                'PORTUGAL - LIGA PORTUGAL 2', 'SUÉCIA - ALLSVENSKAN', 'TURQUIA - SUPER LIG'])
-
-
+selected_league = st.sidebar.selectbox('League', ['ALEMANHA - 2. BUNDESLIGA', 'ALEMANHA - BUNDESLIGA', 'BRASIL - SÉRIE A', 'BRASIL - SÉRIE B', 'BRASIL - SÉRIE C', 'BÉLGICA - LIGA JUPILER', 'ESPANHA - LALIGA', 'ESPANHA - LALIGA2', 'FRANÇA - LIGUE 1', 'INGLATERRA - PREMIER LEAGUE', 'ITÁLIA - SERIE A', 'ITÁLIA - SERIE B', 'JAPÃO - LIGA J1', 'MÉXICO - LIGA MX', 'NORUEGA - ELITESERIEN', 'PORTUGAL - LIGA PORTUGAL', 'PORTUGAL - LIGA PORTUGAL 2', 'SUÉCIA - ALLSVENSKAN', 'TURQUIA - SUPER LIG'])
 
 def load_data(league):
-  
-  #https://github.com/Franciscovj/jogos_dia_nba/blob/main/app/dados_exportados.csv
-   url = f"https://raw.githubusercontent.com/Franciscovj/jogos_dia_nba/f2c3a5bde9becbf90d4ec14709cc3f613aaeeb1d/app/dados_exportados.csv?raw=true"
-   data = pd.read_csv(url)
-
-   data = data[data['League'] == selected_league]
-    
-   return data
+    url = f"https://raw.githubusercontent.com/Franciscovj/jogos_dia_nba/f2c3a5bde9becbf90d4ec14709cc3f613aaeeb1d/app/dados_exportados.csv?raw=true"
+    data = pd.read_csv(url)
+    data = data[data['League'] == league]
+    return data
 
 df = load_data(selected_league)
-
-
 
 # Sidebar - Team selection
 sorted_unique_team = sorted(df.Home.unique())
@@ -41,6 +30,18 @@ selected_team = st.sidebar.multiselect('Teams', sorted_unique_team, sorted_uniqu
 # Filtering data
 df_filtered = df[(df.Home.isin(selected_team))]
 
+# Apply style to the DataFrame
+def highlight_above_threshold(val, threshold):
+    color = 'background-color: yellow' if val > threshold else ''
+    return color
 
-st.subheader('DataFrame - '+selected_league)
-st.dataframe(df_filtered)
+styled_df = df_filtered.style \
+    .applymap(lambda x: highlight_above_threshold(x, 10), subset=['alguma_coluna']) \
+    .set_properties(**{'background-color': 'black', 'color': 'white'}) \
+    .applymap(lambda x: 'background-color: lightgrey', subset=pd.IndexSlice[::2, :]) \
+    .format({'coluna_numerica': '{:.2f}', 'coluna_monetaria': 'R${:.2f}'})
+
+# Display the styled DataFrame
+st.subheader('Styled DataFrame - ' + selected_league)
+st.dataframe(styled_df, width=800)
+
